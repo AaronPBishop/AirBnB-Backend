f452623597da716e7176e42eee4ae0046c79a6dc
AaronPBishop <aaronpbishop@gmail.com>
2022-09-07 20:11:14 -0500

    Updated readme

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Database Schema Design
 
-`<insert database schema design here>`
+See file DB-Schema.md
 
 ## API Documentation
 
--- public/server/app.js
+++
@@ -1,9 +0,0 @@
-require('express-async-errors');
-require('dotenv').config();
-const express = require('express');
-const app = express();
-
-app.use(express.json());
-
-const port = 5000;
-app.listen(port, () => console.log('Server is listening on port', port));
