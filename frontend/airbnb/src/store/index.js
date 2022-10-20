a6701e79e074fc300ed83ce6628f6edc95740804
AaronPBishop <aaronpbishop@gmail.com>
2022-10-20 18:51:04 -0500

    Added SignupForm component, associated reducers, and routes

--- frontend/airbnb/src/store/index.js
+++ frontend/airbnb/src/store/index.js
@@ -2,9 +2,7 @@ import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import sessionReducer from "./session";
 
-const rootReducer = combineReducers({
-  session: sessionReducer
-});
+const rootReducer = combineReducers({session: sessionReducer});
 
 let enhancer;
 
