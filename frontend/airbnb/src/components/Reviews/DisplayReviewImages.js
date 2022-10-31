dad69fa82a6d1b06438366a9614ae3495380e254
AaronPBishop <aaronpbishop@gmail.com>
2022-10-31 13:02:32 -0500

    Added buttons to DisplayReviewImages component which iterate through review images

--- frontend/airbnb/src/components/Reviews/DisplayReviewImages.js
+++ frontend/airbnb/src/components/Reviews/DisplayReviewImages.js
@@ -1,13 +1,28 @@
+import { useState } from 'react';
+
 import './styles.css';
 
 const DisplayReviewImages = ({ imgArray, clicked, imgCount }) => {
+    const [imgIndex, setImgIndex] = useState(0);
+    
     return (
         <div id={clicked ? 'review-images-container' : 'hide-review-images-container'}>
-            {imgArray.map((img, i) => {
-                return  <div>
-                            <img className='review-images' src={img.url} key={i}></img>
-                        </div>
-            })}
+            <img className='review-images' src={imgArray[imgIndex].url}></img>
+            {
+                imgCount > 1 &&
+                <div id='iterate-review-images-div'>
+                    <button className='review-images-iterator' onClick={e => {
+                        e.stopPropagation();
+
+                        if (imgIndex > 0) setImgIndex(imgIndex - 1);
+                    }}>...Previous</button>
+                    <button className='review-images-iterator' onClick={e => {
+                        e.stopPropagation();
+
+                        if (imgIndex < (imgCount - 1)) setImgIndex(imgIndex + 1);
+                    }}>...Next</button>
+                </div>
+            }
         </div>
     );
 };
